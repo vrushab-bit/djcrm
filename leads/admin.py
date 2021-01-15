3a9ce5e931a54e6ba7527bfb5a92bcb23ec32be1
Vrushab <vrushab.ghodke@gmail.com>
2021-01-15 12:11:00 +0530

    0.1

---
+++ leads/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Lead, Agent, User
+
+
+
+admin.site.register(Lead)
+admin.site.register(Agent)
+admin.site.register(User)
+
+
+# Register your models here.
+
+
